35a59803bf3d4a86150083e5e37a67cc73d44a94
Caleb Lammers <caleb.lammers@tbcschools.ca>
2019-05-30 19:02:44 -0400

    Finished scraper

--- Artificial Intelligence/func.py
+++ Artificial Intelligence/func.py
@@ -12,9 +12,89 @@ class AppURLopener(urllib.request.FancyURLopener):
 def get_poem(max_characters, url):
     opener = AppURLopener()
     response = opener.open(url)
+    html = response.read()
 
-    soup = BeautifulSoup(response, 'lxml')
-    text = soup.find_all('Well, read')
-    print(text)
+    s = BeautifulSoup(html, 'html.parser')
+    a=''
+    poem = s.find_all('div', attrs={'style': 'text-indent: -1em; padding-left: 1em;'})
+    for text in poem:
+        b = text.get_text()
+        a += ' '
+        a += b
 
-get_poem(config.max_characters, 'https://www.poetryfoundation.org/poems/149945/cold-valley')
+    a = process_poem(a)
+    print(a)
+
+def process_poem(str):
+    #Remove html formatting
+    str = str.replace("\xa0", '')
+    str = str.replace("\r", '')
+    str = str.replace("', '",'')
+
+    #Remove punctuation
+    str = str.replace(".", '')
+    str = str.replace(",", '')
+    str = str.replace("?", '')
+    str = str.replace("!", '')
+    str = str.replace("'", '')
+    str = str.replace('"', '')
+    str = str.replace(":", '')
+    str = str.replace(";", '')
+    str = str.replace("-", '')
+    str = str.replace("–", '')
+    str = str.replace("—", '')
+    str = str.replace("—", '')
+    str = str.replace("(", '')
+    str = str.replace(")", '')
+    str = str.replace("/", '')
+    str = str.replace("&", 'and')
+
+
+    #Remove extra spaces
+    while "  " in str:
+        str = str.replace("  ", " ")
+
+    #Make lowercase
+    str = str.lower()
+
+    #Remove first character (will always be a space)
+    str = str[1:]
+
+    return str
+
+def get_themes(total_themes, url):
+    opener = AppURLopener()
+    response = opener.open(url)
+    html = response.read()
+
+    s = BeautifulSoup(html, 'html.parser')
+    themes = s.find('meta', attrs={'property': 'article:tag'})
+    text = themes['content']
+
+    themes_list=process_themes(text)
+
+    final_themes = []
+    for i in themes_list:
+        if i in total_themes and i not in final_themes:
+            final_themes.append(i)
+
+    print(final_themes)
+
+
+def process_themes(str):
+    str = str.replace(' ', 'placeholder')
+    str = str.replace(',', ' ')
+
+    list = str.split()
+
+    list = [e.replace('placeholder', ' ') for e in list]
+    list = [e.lower() for e in list]
+
+
+    return list
+
+
+
+
+get_poem(config.max_characters, 'https://www.poetryfoundation.org/poetrymagazine/poems/149694/apostrophe')
+get_themes(config.themes, 'https://www.poetryfoundation.org/poetrymagazine/poems/149694/apostrophe')
