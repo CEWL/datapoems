5b43cbe74e528fe86cef0289648f5db0ad4f56bb
matthewr9 <32723561+matthewr9@users.noreply.github.com>
2019-06-01 13:23:57 -0400

    Finished Base Code For The Main Page

--- website/src/App.js
+++ website/src/App.js
@@ -12,12 +12,22 @@ class App extends React.Component {
                     .datapoems
                 </div>
 
-                <img src="./logo.png" className="logo"/>
+                <img className="logo" src="./logo.png"/>
                        
                 </div>
 
                 <div className="container">
-                
+                    <div className="sloganLineOne">Poem Theme</div>
+                    <div className="sloganLineTwo">Detector</div>
+                    <div className="textBox">
+                        <form action="/action_page.php">
+                        <input className="box" type="text" name="userPoem" placeholder="Please Enter Your Poem Here..."></input>
+                        </form>
+                    </div>
+                    <button class="button">Enter</button>
+                    <img className="leftTriangle" src="./leftTriangle.png"/>
+                    <img className="rightTriangle" src="./rightTriangle.png"/>
+
                 </div>
             </div>
         );
