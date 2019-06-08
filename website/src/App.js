82db77061e6dbb0403b346c8d86294dcc1b2fe21
matthewr9 <32723561+matthewr9@users.noreply.github.com>
2019-06-08 15:09:53 -0400

    Added blur animation on button click

--- website/src/App.js
+++ website/src/App.js
@@ -2,9 +2,15 @@ import React from "react";
 import "./App.scss";
 
 class App extends React.Component {
+
+    state = { fadingOut: false }
+    fadeOut = () => this.setState({fadingOut: true});
+    
+
     render() {
         return (
-            <div className="App">
+            <div className="App" style={this.state.fadingOut ? {filter: 'blur(0.35vw)'} : undefined }
+            > 
 
                 <div className="top-bar">
 
@@ -20,7 +26,7 @@ class App extends React.Component {
                     <div className="sloganLineOne">Poem Theme</div>
                     <div className="sloganLineTwo">Detector</div>
                     <textarea className="inputField" placeholder="Please Enter Your Poem Here..."></textarea>
-                    <button className="button">Enter</button>
+                    <button className="button" onClick={this.fadeOut}>Enter</button>
                     <img className="leftTriangle" src="./leftSideTriangle.png"/>
                     <img className="rightTriangle" src="./rightSideTriangle.png"/>
                 </div>
