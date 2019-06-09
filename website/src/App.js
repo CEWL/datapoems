829ef9e33ed9d573de08a33d6590f525c2182c9b
matthewr9 <32723561+matthewr9@users.noreply.github.com>
2019-06-09 10:44:52 -0400

    Running into loader issues

--- website/src/App.js
+++ website/src/App.js
@@ -2,37 +2,53 @@ import React from "react";
 import "./App.scss";
 
 class App extends React.Component {
-
-    state = { fadingOut: false }
-    fadeOut = () => this.setState({fadingOut: true});
-    
-
+    state = { fadingOut: false };
+    fadeOut = () => this.setState({ fadingOut: true });
+  
     render() {
-        return (
-            <div className="App" style={this.state.fadingOut ? {filter: 'blur(0.35vw)'} : undefined }
-            > 
-
-                <div className="top-bar">
-
-                <div className="title">
-                    .datapoems
-                </div>
-
-                <img className="logo" src="./logo.png"/>
-                       
-                </div>
-
-                <div className="container">
-                    <div className="sloganLineOne">Poem Theme</div>
-                    <div className="sloganLineTwo">Detector</div>
-                    <textarea className="inputField" placeholder="Please Enter Your Poem Here..."></textarea>
-                    <button className="button" onClick={this.fadeOut}>Enter</button>
-                    <img className="leftTriangle" src="./leftSideTriangle.png"/>
-                    <img className="rightTriangle" src="./rightSideTriangle.png"/>
-                </div>
+      const contentStyle = this.state.fadingOut
+        ? { filter: "blur(0.35vw)" }
+        : undefined;
+      const loadingIndicator = this.state.fadingOut ? (
+        <div style={loaderStyle}><div className="loader"/></div>
+      ) : null;
+  
+      return (
+        <div className="App" style={{ position: "relative" }}>
+          <div style={contentStyle}>
+            <div className="top-bar">
+              <div className="title">.datapoems</div>
+              <img className="logo" src="./logo.png" />
             </div>
-        );
+            <div className="container">
+              <div className="sloganLineOne">Poem Theme</div>
+              <div className="sloganLineTwo">Detector</div>
+              <textarea
+                className="inputField"
+                placeholder="Please Enter Your Poem Here..."
+              />
+              <button className="button" onClick={this.fadeOut}>
+                Enter
+              </button>
+              <img className="leftTriangle" src="./leftSideTriangle.png" />
+              <img className="rightTriangle" src="./rightSideTriangle.png" />
+            </div>
+          </div>
+          {loadingIndicator}
+        </div>
+      );
     }
-}
+  }
+  
+  const loaderStyle = {
+    position: "absolute", // or position: "fixed"
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  };
 
 export default App;
